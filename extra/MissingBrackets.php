9d72eff446cae0298d89f7e517c1c9122235c7a5
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2014-01-21 13:40:37 +0100

    Remove extra code.

--- extra/MissingBrackets.php
+++ extra/MissingBrackets.php
@@ -19,32 +19,3 @@ CODE;
 $s = new \atoum\instrumentation\sequence\matching(token_get_all($a));
 
 echo $a, "\n\n\n", $s;
-
-
-
-exit;
-
-//  '<?php' . "\n" .
-//  'class C {' . "\n" .
-//  '    function f ( ) {' . "\n" .
-//  '        if(1)' . "\n" .
-//  //'            if(42)$x++;' . "\n" .
-//  //'            $e++;' . "\n" .
-//  /*
-//  '            if(2)' . "\n" .
-//  '                $a++;' . "\n" .
-//  '            elseif(3)' . "\n" .
-//  */
-//  '                foreach($foo as $r)' . "\n" .
-//  '                    if(4) $x++;' . "\n" .
-//  '                    else $x--;' . "\n" .
-//  //'                $b++;' . "\n" .
-//  '            else' . "\n" .
-//  '                $c++;' . "\n" .
-//  /*
-//  '        else' . "\n" .
-//  '            $a = function ( ) { $i; $j; $k; };' . "\n" .
-//  //'        while(false);' . "\n" .
-//  */
-//  '    }' . "\n" .
-//  '}'
